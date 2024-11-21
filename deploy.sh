5e0402fa8ec4b1e0ba8fcd9b12377666087e60b0
Felix <felix.berthier@epita.fr>
2024-11-21 13:55:01 +0100

    Removed elements from deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ EDGEDB_HOST=edgedb
 EDGEDB_PORT=5656
 EDGEDB_TLS_SECURITY=strict
 EDGEDB_DSN=edgedb://${EDGEDB_USER}:${EDGEDB_PASSWORD}@${EDGEDB_HOST}:${EDGEDB_PORT}/${EDGEDB_DATABASE}?tls_security=${EDGEDB_TLS_SECURITY}
+
 DOMAIN_NAME="" # Add your own
 EMAIL=""       # Add your own
 
@@ -31,53 +32,9 @@ fi
 # Script Vars
 REPO_URL="https://github.com/tofupilot/on-premise.git"
 APP_DIR=~/tofupilot
-SWAP_SIZE="1G"  # Swap size of 1GB
 
 # Update package list and upgrade existing packages
 sudo apt update && sudo apt upgrade -y
-
-# Add Swap Space
-echo "Adding swap space..."
-sudo fallocate -l $SWAP_SIZE /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-
-# Make swap permanent
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-
-# Install Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt update
-sudo apt install docker-ce -y
-
-# Install Docker Compose
-sudo rm -f /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Wait for the file to be fully downloaded before proceeding
-if [ ! -f /usr/local/bin/docker-compose ]; then
-  echo "Docker Compose download failed. Exiting."
-  exit 1
-fi
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Ensure Docker Compose is executable and in path
-sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Verify Docker Compose installation
-docker-compose --version
-if [ $? -ne 0 ]; then
-  echo "Docker Compose installation failed. Exiting."
-  exit 1
-fi
-
-# Ensure Docker starts on boot and start Docker service
-sudo systemctl enable docker
-sudo systemctl start docker
 
 # Clone the Git repository
 if [ -d "$APP_DIR" ]; then
